d0f06734bf99bb62a721ded24f0e68119c8c7658
Isaac Yanez <isaac231467@gmail.com>
2023-02-21 14:00:48 -0600

    90% done with the app just need to add a few more finishing touches

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
-console.log("hi");
+const API = "https://api.adviceslip.com/advice";
+const buttonElement = document.querySelector(".btn-container > button");
+const adviceText = document.querySelector("#advice");
+const adviceID = document.querySelector(".advice-id");
+const dividerImage = document.querySelector(".divider-image");
+
+window.addEventListener("resize", function () {
+  if (this.window.innerWidth <= 590) {
+    dividerImage.src = "./images/pattern-divider-mobile.svg";
+  } else {
+    dividerImage.src = "./images/pattern-divider-desktop.svg";
+  }
+});
+
+function fetchAdvice() {
+  fetch(API)
+    .then((response) => response.json(response))
+    .then((data) => {
+      adviceText.innerHTML = data.slip.advice;
+      adviceID.innerHTML = `#${data.slip.id}`;
+    });
+}
+
+buttonElement.addEventListener("click", fetchAdvice);
