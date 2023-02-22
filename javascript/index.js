596b336d15f1b6747f445a7d2935f098687ddfcf
Isaac Yanez <isaac231467@gmail.com>
2023-02-21 19:02:18 -0600

    fixed a bug

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,7 @@ const adviceID = document.querySelector(".advice-id");
 const dividerImage = document.querySelector(".divider-image");
 
 window.addEventListener("resize", function () {
-  if (this.window.innerWidth <= 590) {
+  if (this.window.innerWidth <= 700) {
     dividerImage.src = "./images/pattern-divider-mobile.svg";
   } else {
     dividerImage.src = "./images/pattern-divider-desktop.svg";
